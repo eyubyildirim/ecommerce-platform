95645cec2386bcfcdf15343b29c4d0565083292e
Eyub Yildirim <eyub2001@gmail.com>
2025-06-27 21:29:25 +0300

    feat: inventory-service main impl

--- cmd/inventory_service/main/main.go
+++ cmd/inventory_service/main/main.go
@@ -1,16 +1,42 @@
 package main
 
 import (
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"ecommerce-platform/internal/database"
+	"ecommerce-platform/services/inventory/api/handler"
+	"ecommerce-platform/services/inventory/repository/postgres"
+	"ecommerce-platform/services/inventory/service"
+	"log/slog"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	_ "github.com/lib/pq"
 )
 
 func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil)).With("service", "product-service")
+
+	db := database.InitDb("postgres_db", "5432", "user", "password", "ecommerce_db")
+	defer db.Close()
+
+	inventoryRepo, err := postgres.NewInventoryPgRepository(db, logger)
+	if err != nil {
+		panic(err)
+	}
+	inventoryService := service.NewInventoryService(inventoryRepo, logger)
+	inventoryHandler := handler.NewInventoryHandler(inventoryService, logger)
+
 	r := chi.NewRouter()
 
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
-	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
+
+	r.Route("/products", func(r chi.Router) {
+		r.Post("/", inventoryHandler.AddProduct)
+		r.Get("/{id}", inventoryHandler.GetPrice)
+	})
+
+	http.ListenAndServe(":8082", r)
 }
